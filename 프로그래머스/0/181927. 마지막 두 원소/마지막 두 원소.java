b679fc417efc3530c0cea063bb82ae03225dc703
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-03-27 17:48:18 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int [num_list.length + 1];
+        
+        int last = num_list[num_list.length - 1];
+        int lastf = num_list[num_list.length - 2];
+        
+        for (int i = 0; i < num_list.length; i++){
+            answer[i] = num_list[i];
+        }if(last > lastf){
+            answer[num_list.length] = last - lastf;
+        }else{
+            answer[num_list.length] = last * 2;
+        }
+        
+        return answer;
+    }
+}
