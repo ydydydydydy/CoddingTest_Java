ca3aa108247ea2361d341a340e8294ae29a6f4d2
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-04-03 17:36:59 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        int a, b, temp;
+        for(int i=0; i<queries.length; i++){
+            a = queries[i][0];
+            b = queries[i][1];
+            
+            temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+        return arr;
+    }
+}
