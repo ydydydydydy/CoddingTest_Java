1192e502ffab94259d05434dac4859acb8bfe5a6
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-04-03 17:47:41 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -13,3 +13,6 @@ class Solution {
         return arr;
     }
 }
+// 변수 a는 queries 배열의 각 행에서 첫 번째 요소인 인덱스를 나타냄. queries[i][0]는 i번째 행의 첫 번째 요소를 의미
+
+// ex) queries 배열의 한 행이 [2, 5]라면, a는 2이고 b는 5이다. 이는 arr 배열에서 2번 인덱스와 5번 인덱스의 요소를 교환하라는 의미
