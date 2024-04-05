388f99843d9469ed3232d7dc27a4b79366346437
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-04-05 15:36:49 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.56 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -2,7 +2,9 @@ class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         for (int i = 0; i < queries.length; i++) {
             for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+            //queries 배열의 요소 i번째에서 시작하는 인덱스 queries[i][0]부터 끝나는 인덱스 queries[i][1]까지 반복
                 if (j % queries[i][2] == 0) arr[j] += 1;
+            //j가 queries 배열의 i번째 요소의 세 번째 값(k)으로 나누어 떨어지는지 확인하고 arr 배열의 해당 인덱스에 1을 더함
             }
         }
         return arr;
