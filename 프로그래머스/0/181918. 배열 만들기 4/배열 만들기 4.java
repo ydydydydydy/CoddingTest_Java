d366fb4a880c30b7f9de279efb2cb9735e723c30
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-04-14 15:22:07 +0900

    Update 배열 만들기 4.java

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -7,11 +7,17 @@ class Solution {
             if(stk.size() == 0){
                 stk.add(arr[i]); i++;
             } else if(stk.size() != 0 && stk.get(stk.size()-1)<arr[i]){
+              // 스택이 비어있지 않고, 스택의 맨 위 요소가 현재 배열 요소보다 작은지 확인
                 stk.add(arr[i]); i++;
+                // 그렇다면 현재 배열 요소를 스택에 추가, 배열 인덱스를 증가
             } else {
                 stk.remove(stk.size() - 1);
+                // 아니라면 스택의 맨 위 요소를 제거
+                // 이는 스택의 LIFO (Last In First Out) 특성에 따라 가장 최근에 추가된 요소를 제거
             }
         }
         return stk.stream().mapToInt(i -> i).toArray();
+        // 스택에 남아있는 요소들을 정수 배열로 변환하여 반환
+        // 스트림을 이용하여 리스트의 각 요소를 정수형으로 매핑하고, toArray 메서드를 사용하여 정수 배열로 변환
     }
 }
