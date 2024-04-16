36aef25775035ef115e956ea1a2c4849e6c2db48
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-04-16 10:23:30 +0900

    Update 주사위 게임 3.java

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -12,6 +12,9 @@ class Solution {
             ans = 1111 * dice [3];
         } else if (dice[0] == dice[2] || dice[1] == dice[3]){
             ans = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+            // Math.pow(): 거듭제곱을 계산하는 함수로 두 개의 매개변수를 받음
+            // 첫 번째 매개변수는 밑(base)이고, 두 번째 매개변수는 지수(exponent)
+            // 즉, Math.pow(a, b)는 a의 b 제곱을 반환
         } else if (dice[0] == dice[1] && dice[2] == dice[3]){
             ans = (dice[0] + dice[3]) * (dice[3] - dice[0]);
         } else if (dice[0] == dice[1]){
