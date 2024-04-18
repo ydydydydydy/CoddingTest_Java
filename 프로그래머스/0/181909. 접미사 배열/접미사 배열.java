d9aedeca55ee3f8273fedb1897bc50b2355e82b4
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-04-18 09:26:55 +0900

    [level 0] Title: 접미사 배열, Time: 0.21 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        int length = my_string.length();
+        // 주어진 문자열의 길이를 구함. 이 길이는 후행 문자열을 만들 때 사용
+        String[] suffixes = new String[length];
+        // 주어진 문자열의 길이만큼의 공간을 가지는 문자열 배열을 생성
+        // 이 배열에는 주어진 문자열의 모든 후행 문자열이 저장
+
+        for(int i = 0; i < length; i++){
+            suffixes[i] = my_string.substring(i);
+            // 현재 인덱스부터 주어진 문자열의 끝까지의 후행 문자열을 구해 suffixes 배열에 저장
+        }
+  
+        Arrays.sort(suffixes);
+        // 생성된 후행 문자열들을 알파벳 순으로 정렬
+        return suffixes;
+    }
+}
