96ced13d32d8d7f3ff04124899b9f6c442c0a65b
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-04-24 09:31:21 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        if(my_string.endsWith(is_suffix)){
+        // endsWith(): 대상 문자열이 특정 문자 또는 문자열로 끝나는지 체크하는 함수
+            return 1;
+        }
+        return answer;
+    }
+}
