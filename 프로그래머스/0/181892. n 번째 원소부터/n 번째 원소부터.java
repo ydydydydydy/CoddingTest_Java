04032cabe26b30024b9a13c3c34ee5d307cb0d84
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-05-16 15:38:54 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length - n + 1];
+        
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = num_list[n-1];
+            n++;  // n을 1씩 증가시켜 answer[i+1]에는 num_list[n]이 오도록 함
+        }
+        
+        return answer;
+    }
+}
