e0a67c8a5b51561ca9275bbf311d69e6f484bd3b
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-05-22 10:11:59 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[n];
-        
+        //  int[] answer = new int[n];는 크기가 n인 새로운 정수형 배열을 만들고 answer 변수에 할당
+        // n이 5라면 answer는 5개의 정수형 요소를 가지는 배열이 되고, 초기 값은 모두 0이 된다
         for(int i = 0; i < n; i++){
             answer[i] = num_list[i];
         }
