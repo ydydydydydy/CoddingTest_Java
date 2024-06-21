1a21ec39c0b42bece3e6d856a2de8ba1f2afef82
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-06-21 20:16:22 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -7,6 +7,9 @@ class Solution {
             
             int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             // copyOfRange - 지정한 배열에서 특정 범위만큼의 요소들을 복사해 새로운 배열로 반환
+            // commands[i][0]는 commands 배열의 i번째 요소의 첫 번째 값을 의미
+            //commands[i][1]는 commands 배열의 i번째 요소의 두 번째 값을 의미
+            
             // i - 1번째 수부터 j 직전까지 배열 추출
             Arrays.sort(temp);
             // 추출한 배열 오름차순 정렬
