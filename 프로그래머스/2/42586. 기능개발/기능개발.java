b4fad58c08bf084bb72193671e6547be3536bc4c
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-07-02 19:32:32 +0900

    [level 2] Title: 기능개발, Time: 5.88 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
-        int[] dayOfend = new int[100];
+        int[] end = new int[100];
+        // end = 각 일자별로 완료된 작업수를 저장할 배열
         int day = -1;
+        // 작업 완료 일자를 계산하기 위한 변수
         for(int i=0; i<progresses.length; i++){
             while(progresses[i] + (day*speeds[i]) < 100){
                 day++;
             }
-            dayOfend[day]++;
+            // progresses[i] + (day*speeds[i]) < 100 조건을 만족할 때까지 day 증가
+            end[day]++;
+            // 완료된 작업의 개수를 해당 일자(day)의 인덱스에 증가
         }
-        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+        return Arrays.stream(end).filter(i -> i!=0).toArray();
+        // end 배열에서 0이 아닌 값들만 필터링하여 배열로 변환 후 반환
+        // 완료된 작업이 있는 일자만 결과에 포함시키기 위함
     }
 }
