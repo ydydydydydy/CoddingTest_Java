2a6fa0e5203babcf3e55b71c3670f10d457dceff
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-08-28 23:52:31 +0900

    [level 3] Title: 숫자 게임, Time: 61.08 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -8,10 +8,17 @@ class Solution {
         for(int i = 0; i < B.length; i++){
             if(A[idx] >= B[i]) continue;
             idx++;
-            // B[i] 값이 A[idx]보다 작거나 같을 경우 나머지 코드를 건너뛰고 다음 i로 넘어감
-            // B[i]값이 A[idx]보다 큰 경우 A의 idx 증가
-            // -> A배열에서 B의 현재 원소보다 작은 값을 하나 더 찾음
+            // B를 기준으로 순회
+            // 현재 B값이 A보다 같거나 작을 경우 B의 다음 index 체크
+            // 현재 B값이 A보다 큰 경우를 A의 index를 증가 
         }
         return idx;
     }
 }
+// A: 1, 3, 5, 7
+// B: 2, 2, 6, 8
+// 2 -> 6 -> 8 
+// 2는 3보다 작으므로 skip 
+// 순회가 끝난 후 A의 index 반환
+
+ 
