bb98af606e310eb9b60937ad72772aaa9d96a725
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-11-26 17:13:28 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.14 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,7 +1,6 @@
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = {};
-        answer = my_string.split(" ");
+        String[] answer = my_string.split(" ");
         return answer;
     }
 }
