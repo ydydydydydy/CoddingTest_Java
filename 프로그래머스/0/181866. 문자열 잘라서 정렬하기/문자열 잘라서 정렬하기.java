8ddeef3227d7bd0c83d7d565f63cedcb0c7ef5e7
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2024-12-18 17:40:29 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 24.20 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -5,6 +5,7 @@ class Solution {
     public String[] solution(String myString) {
         String[] splitArr = myString.split("x");
         ArrayList<String> resultList = new ArrayList<>();
+        // resultList를 toArray 메서드를 사용하여 다시 배열로 변환
         
         for (String str : splitArr) {
             if (!str.isEmpty()) {
@@ -14,6 +15,7 @@ class Solution {
         
         String[] resultArr = resultList.toArray(new String[resultList.size()]);
         Arrays.sort(resultArr);
+        // sort -> 정렬
      
         return resultArr;
     }
