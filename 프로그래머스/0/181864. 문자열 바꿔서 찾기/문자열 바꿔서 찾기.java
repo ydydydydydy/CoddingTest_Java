8a64b104e01f14735e1cff9ed69bda59a36d240d
염다영 <140699908+ydydydydydy@users.noreply.github.com>
2025-01-10 18:59:55 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,14 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
-        String text;
-        text = myString.replace('A', 'C');
-        text = text.replace('B', 'A');
-        text = text.replace('C', 'B');
-        if(text.contains(pat)){
-            answer = 1;
-        }
-        
-        return answer;
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
     }
 }
